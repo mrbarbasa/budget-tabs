97ab922abc85dddc059401b2bc005630af5c7525
Marifel Barbasa <mrbarbasa@gmail.com>
2017-11-12 09:00:09 -1000

    Utilize the fonts on the Overview screen

--- containers/Overview.js
+++ containers/Overview.js
@@ -3,15 +3,17 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 
 const Overview = ({ navigation }) => (
   <View style={styles.container}>
-    <View style={{flex: 1, backgroundColor: 'powderblue'}}>
+    <View style={{ flex: 1, backgroundColor: 'powderblue' }}>
       <Text style={styles.header}>Budget Tabs</Text>
+      <Text style={styles.totalExpenses}>3721.11</Text>
+    </View>
+    <View style={{ flex: 2, backgroundColor: 'skyblue' }}>
       <Button
         title="Add Expense"
         onPress={() => navigation.navigate('AddExpense')}
       />
     </View>
-    <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-    <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+    <View style={{ flex: 3, backgroundColor: 'steelblue' }} />
   </View>
 );
 
@@ -20,8 +22,13 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   header: {
-    fontSize: 24,
-    fontWeight: '600',
+    fontFamily: 'Source Sans Pro',
+    fontSize: 30,
+    textAlign: 'center',
+  },
+  totalExpenses: {
+    fontFamily: 'Source Code Pro',
+    fontSize: 30,
     textAlign: 'center',
   },
 });
